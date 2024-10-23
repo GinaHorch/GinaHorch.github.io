98d64b7929caac67e9595040e93f112faa3207c4
Gina Horch <reggiaus@hotmail.com>
2024-10-24 07:32:14 +0800

    Performance improvements and styling improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-import '/assets/css/style.css'
+import '../css/style.css'
+
+const summary = document.querySelector('summary');
+summary.addEventListener('click', () => {
+  const expanded = summary.getAttribute('aria-expanded') === 'true' || false;
+  summary.setAttribute('aria-expanded', !expanded);
+});
