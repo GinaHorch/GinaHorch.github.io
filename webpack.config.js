661d31e9943708da6b3881651e572b2efffdc568
GinaHorch <reggiaus@hotmail.com>
2024-10-25 18:14:26 +0800

    Set dynamic modification - depending on development or production

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,35 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: 'production',
-    entry: './assets/js/script.js',  // Your JS entry file
-    output: {
-    filename: 'script.min.js',  // Minified JS output file
-    path: path.resolve(__dirname, 'dist'),  // Output folder
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],  // CSS loader
-      },
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        mode: isProduction ? 'production': 'development',
+        entry: './assets/js/script.js',  // Your JS entry file
+        output: {
+            filename: 'script.min.js',  // Minified JS output file
+            path: path.resolve(__dirname, 'dist'),  // Output folder
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],  // CSS loader
+        },
+      ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: isProduction ? 'style.min.css' : 'style.css',
+        }),
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-        filename: 'style.min.css'
-    }),
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin(),  // Minifies JS
-      new CssMinimizerPlugin(),  // Minifies CSS
-    ],
-  },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(),  // Minifies JS
+            new CssMinimizerPlugin(),  // Minifies CSS
+        ],
+    },
+  };
 };
